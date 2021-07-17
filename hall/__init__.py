b0343bc9a0c7e7d6a2f469986f80c7e830fcb7d4
jorenham <jhammudoglu@gmail.com>
2021-07-18 01:28:58 +0200

    cleaner imports, and pretty mpmath `repr`

--- hall/__init__.py
+++ hall/__init__.py
@@ -1,3 +1,7 @@
-from . import continuous, discrete
 from ._core import Distribution, Stochast
+from .backend import *
+from .continuous import *
+from .discrete import *
 from .stats import *
+
+mp_configure()
--- hall/backend.py
+++ hall/backend.py
@@ -1,12 +1,40 @@
-__all__ = ["MPBackend", "get_backend"]
+__all__ = ["MPBackend", "get_backend", "mp_configure"]
 
-from mpmath import libmp
+from typing import Final, Tuple, Type, TypeVar
+
+import mpmath
+from mpmath import ctx_base, libmp
 from typing_extensions import Literal
 
 
 MPBackend = Literal["python", "gmpy", "sage"]
 
+MP_CONTEXTS: Final[Tuple[mpmath.ctx_base.StandardBaseContext, ...]] = (
+    mpmath.fp,
+    mpmath.mp,
+    mpmath.iv,
+)
+
 
 def get_backend() -> MPBackend:
     backend: MPBackend = libmp.BACKEND
     return backend
+
+
+def mp_configure(
+    *,
+    prec: int = 53,
+    dps: int = 15,
+    pretty: bool = True,
+) -> None:
+
+    """Configure all mpmath contexts.
+
+    :param prec: binary precision in bits
+    :param dps: decimal precision
+    :param pretty: pretty formatting for `repr()`
+    """
+    for context in MP_CONTEXTS:
+        context.prec = prec
+        context.dps = dps
+        context.pretty = pretty
